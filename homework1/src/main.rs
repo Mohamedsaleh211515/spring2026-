a4a5cfd8ffd8ba2c6eb11197a8f0ab8344c99977
Mohamedsaleh211515 <mohamed.saleh01@utrgv.edu>
2026-02-05 18:07:24 +0000

    homework template NOT DONE

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+fn ass1(){
+   fn assignment1() {
+    let mut temp_f: i32 = 32;
+
+    let temp_c = fahrenheit_to_celsius(temp_f);
+    println!("{temp_f}°F = {temp_c:.2}°C");
+
+    for _ in 0..5 {
+        temp_f += 1.0;
+        let temp_c = fahrenheit_to_celsius(temp_f);
+        println!("{temp_f}°F = {temp_c:.2}°C");
+    }
+   }
+}
+
+fn ass2(){
+    println!("Run results")
+}
+
+fn ass3(){
+    println!("Run results")
+}
+
+fn main(){
+    fn ass1();
 }
